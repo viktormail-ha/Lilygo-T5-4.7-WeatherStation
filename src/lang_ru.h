f1a9698cef41d4672e5b46c140e09c39ed6f5f5a
viktormail <96046282+viktormail-ha@users.noreply.github.com>
2026-08-13 18:57:31 +0300

    Add files via upload

---
+++ src/lang_ru.h
@@ -0,0 +1,68 @@
+#define FONT(x) x##_tf
+
+//Temperature - Humidity - Forecast
+const String TXT_TEMPERATURE_C    = "Температура (°C)";
+const String TXT_TEMPERATURE_F    = "Температура (°F)";
+const String TXT_HUMIDITY_PERCENT = "Влажность (%)";
+const String TXT_HILO             = "Макс|Мин: ";
+const String TXT_FEELSLIKE        = "Ощущ: ";
+
+// Pressure
+const String TXT_PRESSURE_HPA     = "Давление (гПа)";
+const String TXT_PRESSURE_MM      = "Давление (мм)";
+const String TXT_PRESSURE_IN      = "Давление (дюйм)";
+
+//RainFall / SnowFall
+const String TXT_RAINFALL_MM      = "Дождь (мм)";
+const String TXT_RAINFALL_IN      = "Дождь (дюйм)";
+const String TXT_SNOWFALL_MM      = "Снегопад (мм)";
+const String TXT_SNOWFALL_IN      = "Снегопад (дюйм)";
+const String TXT_PRECIP_MM        = "Осадки (мм)";
+const String TXT_PRECIP_IN        = "Осадки (дюйм)";
+const String TXT_NO_PRECIP        = "Нет осадков";
+
+//Moon
+const String TXT_MOON_NEW             = "Новолуние";
+const String TXT_MOON_WAXING_CRESCENT = "Растущий Серп";
+const String TXT_MOON_FIRST_QUARTER   = "Первая Четверть";
+const String TXT_MOON_WAXING_GIBBOUS  = "Растущая Луна";
+const String TXT_MOON_FULL            = "Полнолуние";
+const String TXT_MOON_WANING_GIBBOUS  = "Убывающая Луна";
+const String TXT_MOON_THIRD_QUARTER   = "Третья Четверть";
+const String TXT_MOON_WANING_CRESCENT = "Убывающий Серп";
+
+//Wind
+const String TXT_N   = "С";
+const String TXT_NNE = "ССВ";
+const String TXT_NE  = "СВ";
+const String TXT_ENE = "ВСВ";
+const String TXT_E   = "В";
+const String TXT_ESE = "ВЮВ";
+const String TXT_SE  = "ЮВ";
+const String TXT_SSE = "ЮЮВ";
+const String TXT_S   = "Ю";
+const String TXT_SSW = "ЮЮЗ";
+const String TXT_SW  = "ЮЗ";
+const String TXT_WSW = "ЗЮЗ";
+const String TXT_W   = "З";
+const String TXT_WNW = "ЗСЗ";
+const String TXT_NW  = "СЗ";
+const String TXT_NNW = "ССЗ";
+
+// Moon event
+const String TXT_MOON_RISE   = "Восх";   // Use short names
+const String TXT_MOON_SET    = "Зак";    // Use short names
+const String TXT_MOON_ALT    = "Выс";    // Use short names
+
+// UV
+const String TXT_UV_LOW = "(Низ)";
+const String TXT_UV_MEDIUM = "(Ср)";
+const String TXT_UV_HIGH = "(Выс)";
+const String TXT_UV_VERYHIGH = "(Оч выс)";
+const String TXT_UV_EXTREME = "(Экстр)";
+
+//Day of the week
+const char* weekday_D[] = { "ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ" };
+
+//Month
+const char* month_M[] = { "Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" };
